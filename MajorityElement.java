726389ad409607d093735caa2254a9d1ad44ae6c
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2020-05-11 15:28:50 +0530

    6th May Solution Updated (Optimised Version Added)

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,22 +1,53 @@
+/* 
+Initial Solution - Time = O(N) ; Space O(N)
+
+	class Solution {
+		public int majorityElement(int[] nums) {
+			int size = nums.length;
+			int resultantElement = 0;
+			Map<Integer,Integer> frequency = new HashMap<Integer,Integer>();
+			for(int num : nums) {
+				if(!frequency.containsKey(num)) {
+					frequency.put(num,1);
+				} else {
+					int currentCount = frequency.get(num);
+					frequency.put(num,currentCount+1);
+				}
+			}
+			for(int num : frequency.keySet()) {
+				if(frequency.get(num) > (size/2)) {
+					resultantElement = num;
+					break;
+				}
+			}
+			return resultantElement;
+		}
+	}
+*/
+
+
+/*
+New Solution using Moore's Voting Algorithm
+Time = O(N)
+Space = O(1)
+*/
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int size = nums.length;
-        int resultantElement = 0;
-        Map<Integer,Integer> frequency = new HashMap<Integer,Integer>();
-        for(int num : nums) {
-            if(!frequency.containsKey(num)) {
-                frequency.put(num,1);
+        int majorityElementIndex = 0;
+        int count = 1;
+        int length = nums.length;
+        for(int i=1; i<length; ++i) {
+            if(nums[i] == nums[majorityElementIndex]) {
+                ++count;
             } else {
-                int currentCount = frequency.get(num);
-                frequency.put(num,currentCount+1);
+                --count;
+            }
+            if(count == 0) {
+                majorityElementIndex = i;
+                count = 1;
             }
         }
-        for(int num : frequency.keySet()) {
-            if(frequency.get(num) > (size/2)) {
-                resultantElement = num;
-                break;
-            }
-        }
-        return resultantElement;
+        return nums[majorityElementIndex];
     }
 }
