98e314e4af2f1c0bca9d5fc731f14d8f8cfd9d70
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2020-05-13 08:22:24 +0530

    12th May Optimal Solution Added

--- SingleElementInASortedArray.java
+++ SingleElementInASortedArray.java
@@ -9,6 +9,37 @@ class Solution {
 }
 
 /*
+Optimal Solution using Binary Search O(lg(N))
+
+Idea : Since the array is sorted and have each element repeated twice that must mean that if we divide the array in half, 
+			if every element on the left - sub array is repeated twice  then  we  must find   arr[even_index] == arr[odd_index]
+				i.e the pair starts from even indices (0,1) (2,3) (4,5). Therefore  we  find  the  even_index of mid and check whether
+					the pair exists, if it exits then left-sub array only contains repeated pairs (since the order is followed even == odd)
+						if that is not the case then the one unique element is present in left sub array [0 - mid] which is making the order fail.
+							Same is the case with right sub-array. Hence Binary Search is the optimal solution.
+							
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            if(mid%2 == 1) {
+                --mid;
+            }
+            if(nums[mid] != nums[mid+1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+}
+
+*/
+
+/*
 MY ALTERNATE SOLUTION
 
 Since the array is sorted, we can just traverse from left to right to find the unique number
