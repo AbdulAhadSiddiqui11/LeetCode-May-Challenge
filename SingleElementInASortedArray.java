da8be215aa01f92d603f03f3aac893031b0b3d4e
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2020-05-13 07:54:32 +0530

    12th May Alternate Solution Added

--- SingleElementInASortedArray.java
+++ SingleElementInASortedArray.java
@@ -7,3 +7,27 @@ class Solution {
         return singleElement;
     }
 }
+
+/*
+MY ALTERNATE SOLUTION
+
+Since the array is sorted, we can just traverse from left to right to find the unique number
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int previous = nums[0];
+        int i = 1;
+        int size = nums.length;
+        while(i < size-1) {
+            int current = nums[i];
+            if(previous == current) {
+                previous = nums[++i];
+                i++;
+            } else {
+                return previous;
+            }
+        }
+        return previous;
+    }
+}
+*/
